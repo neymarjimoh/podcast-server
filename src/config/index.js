1511eda73c064869efd814d625e82994a6e2bbd9
neymarjimoh <jemohkunle2007@gmail.com>
2020-07-20 10:28:43 +0100

    addded mongo atlas uri

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 module.exports = {
     PORT: 7500,
-    MONGODB_URI: process.env.MONGODB_URI,
+    MONGODB_URI: 'mongodb+srv://test:test@cluster0.tsp89.mongodb.net/podcast-management-api?retryWrites=true&w=majority',
     JWT_SECRET: 'accesssmetst6667hjjsbfdxgcghvsdlknsdknbkjjknkcsazbcoeuw96',
 };
